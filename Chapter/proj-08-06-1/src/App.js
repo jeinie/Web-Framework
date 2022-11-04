0d09f575562ba592578694f631a2dcf41fc4559a
장정윤 <jeinie9179@naver.com>
2022-11-05 00:14:11 +0900

    데이터 렌더링 및 react-window

--- Chapter/proj-08-06-1/src/App.js
+++ Chapter/proj-08-06-1/src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import bigList from './data/bigList';
-import List from './component/List';
+import {FixedSizeList} from "react-window"
 
-function App() {
-  const renderItem = item => (
-    <div style={{ display: "flex" }}>
-      <img src={item.avatar} alt={item.name} width={50} />
-      <p>{item.name} - {item.email}</p>
+const App = () => {
+  const renderRow = ({ index, style }) => (
+    <div style={{ ...style, ...{display: "flex"}}}>
+      <img src={bigList[index].avatar} alt={bigList[index].name} width={50}/>
+      <p>{bigList[index].name} - {bigList[index].name}</p>
     </div>
   )
-  return <List data={bigList} renderItem={renderItem}/>
+
+  return (
+    <FixedSizeList 
+    height={window.innerHeight-20}
+    width={window.innerWidth-20}
+    itemCount={bigList.length} itemSize={50}>
+      {renderRow}
+    </FixedSizeList>
+  )
 }
 
 export default App;
