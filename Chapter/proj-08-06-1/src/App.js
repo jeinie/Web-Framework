453d7de7245d2befe7ab35da70957304d0e758c4
장정윤 <jeinie9179@naver.com>
2022-11-05 00:06:50 +0900

    데이터 렌더링

--- Chapter/proj-08-06-1/src/App.js
+++ Chapter/proj-08-06-1/src/App.js
@@ -1,14 +1,16 @@
-import bigList from "./data/bigList";
-import List from "./component/List";
+import logo from './logo.svg';
+import './App.css';
+import bigList from './data/bigList';
+import List from './component/List';
 
-const App = () => {
-    const renderItem = item => (
-        <div style={{ display: "flex" }}>
-            <img src={item.avatar} alt={item.name} width={50}/>
-            <p>{item.name} - {item.email}</p>
-        </div>
-    )
-    return <List data={bigList} renderItem={renderItem}/>
+function App() {
+  const renderItem = item => (
+    <div style={{ display: "flex" }}>
+      <img src={item.avatar} alt={item.name} width={50} />
+      <p>{item.name} - {item.email}</p>
+    </div>
+  )
+  return <List data={bigList} renderItem={renderItem}/>
 }
 
 export default App;
